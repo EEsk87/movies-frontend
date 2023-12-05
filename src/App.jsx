5553f7dd776772f7b10eb89c0cd0f7e978e70e7d
Earl Eskridge <EJay@Cindys-Air.lan>
2023-12-05 18:54:41 -0500

    added movie info props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import MovieList from "./components/movie list/movie list";
+import MovieInfo from "./components/movie info/movie info";
 
 function App() {
-  return <div className="App"> 
-  <Header />
-  <MovieList />
-  </div>;
+  return (
+    <div className="App">
+      <Header />
+      <MovieList />
+      <MovieInfo
+        title="Forrest Gump"
+        runningTime={142}
+        genre="Romantic Drama"
+      />
+    </div>
+  );
 }
 
 export default App;
